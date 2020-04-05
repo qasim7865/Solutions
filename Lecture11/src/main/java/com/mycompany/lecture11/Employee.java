5b97d2e90adb8b2c791a3f567495f59cbbae5071
princ <princ@DESKTOP-V3MTBIA>
2020-04-06 01:04:50 +0500

    Mahnoor khizar's assignment

---
+++ Lecture11/src/main/java/com/mycompany/lecture11/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lecture11;
+
+/**
+ *
+ * @author princ
+ */
+public class Employee extends Person{
+
+    public Employee() {
+        System.out.println("Inside Employee's constructor");
+    }
+    
+}
---
+++ Lecture11/src/main/java/com/mycompany/lecture11/Person.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lecture11;
+
+/**
+ *
+ * @author princ
+ */
+public class Person {
+
+    public Person() {
+        System.out.println("Inside Person's constructor");
+    }
+    
+}
